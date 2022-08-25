7745c29cfd14efe20953e0dd4a8801861cb34201
jeffreyaven <javen@avensolutions.com>
2022-08-25 12:47:57 +1000

    updated tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,5 @@
-terraform {
-  backend "gcs" {
-    bucket  = "tf-state-6x3ctx9vj9"
-    prefix  = "terraform/state"
-  }
-}
-
 provider "google" {
   project = var.project
   region  = var.region
   zone    = var.zone
-}
-
-resource "google_storage_bucket" "raw_data" {
-  name          = var.data_bucket
-  location      = var.region
-}
-
-resource "google_composer_environment" "composer" {
-  name   = "my-composer-env"
-  region = var.region
- config {
-    software_config {
-      image_version = "composer-1-airflow-2"
-    }
-  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,3 @@ variable "zone" {
     type = string
     default = "australia-southeast1-a"
 }
-
-variable "data_bucket" {
-    type = string
-}
